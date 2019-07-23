48f2b5aa8ad2d22828dd4273adedb737f01ce106
Eugene Obrezkov <ghaiklor@gmail.com>
2019-07-23 18:40:35 +0300

    Done with assembly code for Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,72 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(BEGIN)
+  // if KEYBOARD == 0 jump to white else jump to black
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+  @BLACK
+  0;JMP
+
+(DRAW_LOOP)
+  // i = 8192, addr = 16384
+  @8192
+  D=A
+  @i
+  M=D
+
+  @SCREEN
+  D=A
+  @addr
+  M=D
+
+(DRAW_LOOP_BODY)
+  // if i == 0 jump to begin
+  @i
+  D=M
+  @BEGIN
+  D;JEQ
+
+  // D = R0 (color to fill)
+  @R0
+  D=M
+
+  // A = addr, [A] = D (R0)
+  @addr
+  A=M
+  M=D
+
+  // addr = addr + 1
+  @addr
+  D=M+1
+  M=D
+
+  // i = i - 1
+  @i
+  D=M
+  D=D-1
+  M=D
+
+  @DRAW_LOOP_BODY
+  0;JMP
+
+(BLACK)
+  // R0 = -1
+  @R0
+  M=-1
+  @DRAW_LOOP
+  0;JMP
+
+(WHITE)
+  // R0 = 0
+  @R0
+  M=0
+  @DRAW_LOOP
+  0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,59 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// if R0 == 0 jump to zero
+@R0
+D=M
+@ZERO
+D;JEQ
+
+// if R1 == 0 jump to zero
+@R1
+D=M
+@ZERO
+D;JEQ
+
+// R2 = R0
+@R0
+D=M
+@R2
+M=D
+
+// i = R1 - 1
+@R1
+D=M
+@i
+M=D-1
+
+(LOOP)
+  // if i == 0 jump to end
+  @i
+  D=M
+  @END
+  D;JEQ
+
+  // R2 = R2 + R0
+  @R2
+  D=M
+  @R0
+  D=D+M
+  @R2
+  M=D
+
+  // i = i - 1
+  @i
+  D=M
+  D=D-1
+  M=D
+
+  // jump back to the loop
+  @LOOP
+  0;JMP
+
+(ZERO)
+  // if some of the operands are zero, R2 = 0
+  @R2
+  M=0
+(END)
+  @END
+  0;JMP
