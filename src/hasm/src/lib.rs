b2042f552170773c12df2c744875c78fa05803ab
Eugene Obrezkov <ghaiklor@gmail.com>
2019-08-07 15:15:55 +0300

    Add messages to panic for read\write files

--- src/hasm/src/lib.rs
+++ src/hasm/src/lib.rs
@@ -9,12 +9,12 @@ use std::fs;
 use std::io::prelude::*;
 
 pub fn run(config: Config) {
-    let assembly_code = fs::read_to_string(config.input_file).unwrap();
+    let assembly_code = fs::read_to_string(config.input_file).expect("Could not read input file");
     let instructions = parser::parse(&assembly_code);
     let symbol_table = symbols::resolve(&instructions);
     let machine_code = codegen::codegen(&instructions, &symbol_table);
 
-    let mut out_file = fs::File::create(config.output_file).unwrap();
+    let mut out_file = fs::File::create(config.output_file).expect("Could not open output file");
     out_file
         .write_all(machine_code.as_bytes())
         .expect("Could not write to the output file");
