522fede62e4f5567831426051ae23f07dc85cfeb
Eugene Obrezkov <ghaiklor@gmail.com>
2019-08-16 16:36:00 +0300

    Remove silence for output in run_tests

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -6,7 +6,7 @@ function assembly_tests() {
     echo "Assembling examples/cpu..."
     for asm_file in "$PWD"/examples/cpu/**/*.asm; do
         echo "Assembling $asm_file..."
-        "$PWD"/tools/assembler.sh "$asm_file" > /dev/null 2>&1
+        "$PWD"/tools/assembler.sh "$asm_file"
     done
 
     echo "Running tests for examples/cpu..."
@@ -17,7 +17,7 @@ function assembly_tests() {
         fi
 
         echo "Running $tst_file in cpu emulator..."
-        "$PWD"/tools/cpu_emulator.sh "$tst_file" > /dev/null 2>&1
+        "$PWD"/tools/cpu_emulator.sh "$tst_file"
     done
 }
 
@@ -30,7 +30,7 @@ function hardware_tests() {
         fi
 
         echo "Running $tst_file in hardware simulator..."
-        "$PWD"/tools/hardware_simulator.sh "$tst_file" > /dev/null 2>&1
+        "$PWD"/tools/hardware_simulator.sh "$tst_file"
     done
 }
 
